c88d830228b6339b3a932c7d782399e5ef470b1f
Moomal <moomalmsoomro@gmail.com>
2020-11-22 23:29:57 -0500

    commit tree 4e88bf99b0083efec8f47340bab49c2564026e4c

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
-# Add  code here!
+# requires integer
+# returns boolean of whether or not the integer is a prime number
+def prime?(num)
+    return false if num <= 1
+    Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+    true
+end
